c8ec4c6b7e20575101f9d077871d3796c430a01f
H A R S H  ! <145217050+Harshbhagat22@users.noreply.github.com>
2024-12-02 12:43:43 +0530

    Add files via upload

---
+++ src/components/Projects.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>My Projects</h2>
+      <ul>
+        <li>
+          Password Generator - <a href="https://github.com/Harshbhagat22/password-generator" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+        </li>
+        <li>
+          Tic-Tac-Toe Game - <a href="https://github.com/Harshbhagat22/tic-tac-toe" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+        </li>
+        <li>
+          Advanced Calculator - <a href="https://github.com/Harshbhagat22/advanced-calculator" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Projects;
